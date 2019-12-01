f9b7b8da51db98f1e3028b74c9b731cd5720fab0
Imesh Sachinda <42859952+imeshsps@users.noreply.github.com>
2019-12-01 14:26:59 +0530

    Add files via upload

---
+++ demo_workers.js
@@ -0,0 +1,60 @@
+var i = 0;
+
+function memorySizeOf(obj) {
+    var bytes = 0;
+
+    function sizeOf(obj) {
+        if(obj !== null && obj !== undefined) {
+            switch(typeof obj) {
+            case 'number':
+                bytes += 8;
+                break;
+            case 'string':
+                bytes += obj.length * 2;
+                break;
+            case 'boolean':
+                bytes += 4;
+                break;
+            case 'object':
+                var objClass = Object.prototype.toString.call(obj).slice(8, -1);
+                if(objClass === 'Object' || objClass === 'Array') {
+                    for(var key in obj) {
+                        if(!obj.hasOwnProperty(key)) continue;
+                        sizeOf(obj[key]);
+                    }
+                } else bytes += obj.toString().length * 2;
+                break;
+            }
+        }
+        return bytes;
+    };
+
+    function formatByteSize(bytes) {
+        if(bytes < 1024) return bytes + " bytes";
+        else if(bytes < 1048576) return(bytes / 1024).toFixed(3) + " KiB";
+        else if(bytes < 1073741824) return(bytes / 1048576).toFixed(3) + " MiB";
+        else return(bytes / 1073741824).toFixed(3) + " GiB";
+    };
+
+    return sizeOf(obj);
+};
+
+function timedCount() {
+
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://nxcom.000webhostapp.com/data2robot.php?id=0');
+  xhr.onload = function() {
+
+  postMessage(memorySizeOf(this.response));
+   }; 
+  xhr.send();
+
+  setTimeout("timedCount()",2000);
+}
+
+timedCount(); 
+
+
+
+
